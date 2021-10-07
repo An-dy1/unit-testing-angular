3f0ac2d35f8d16428e376fc19f7cc8d08d36f919
Andy <andrea_sue2@icloud.com>
2021-10-07 15:05:32 -0500

    first fake test

--- src/app/first-test.spec.ts
+++ src/app/first-test.spec.ts
@@ -5,4 +5,15 @@ describe('first test', () => {
     // set system under test to an empty object so that it is reset before every test
     systemUnderTest = {};
   });
+
+  it('should be true if true', () => {
+    // arrange
+    systemUnderTest.property = false;
+
+    // act
+    systemUnderTest.property = true;
+
+    // assert
+    expect(systemUnderTest.property).toBe(true);
+  });
 });
