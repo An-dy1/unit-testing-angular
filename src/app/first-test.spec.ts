dc088826569ad4159bfc26f14d741dc06b3c5aad
Andy <andrea_sue2@icloud.com>
2021-10-07 14:56:58 -0500

    initial commit

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,8 @@
+describe('first test', () => {
+  let systemUnderTest;
+
+  beforeEach(() => {
+    // set system under test to an empty object so that it is reset before every test
+    systemUnderTest = {};
+  });
+});
