a43c6bfc100ed08a66d1913733aee4487223f21e
Andy <andrea_sue2@icloud.com>
2021-10-08 11:21:25 -0500

    shallow test with debug element

--- src/app/heroes/hero.component.shallow.spec.ts
+++ src/app/heroes/hero.component.shallow.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { HeroComponent } from '../hero/hero.component';
 
 describe('HeroComponent shallow test', () => {
@@ -12,11 +13,27 @@ describe('HeroComponent shallow test', () => {
       schemas: [NO_ERRORS_SCHEMA],
     });
     fixture = TestBed.createComponent(HeroComponent);
+  });
 
-    it('should have correct hero', () => {
-      fixture.componentInstance.hero = { id: 1, name: 'Flimbus', strength: 13 };
-      expect(fixture.componentInstance.hero.name).toEqual('Flimbus');
-      expect(fixture.componentInstance.hero.strength).toEqual(13);
-    });
+  it('should have correct hero', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'Flimbus', strength: 13 };
+    expect(fixture.componentInstance.hero.name).toEqual('Flimbus');
+    expect(fixture.componentInstance.hero.strength).toEqual(13);
+  });
+
+  it('should render hero name in an anchor tag', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'Flimbus', strength: 13 };
+
+    // tells the component to update bindings that may exist on the component
+    fixture.detectChanges();
+
+    // native element exposes browser's DOM api
+    // expect(fixture.nativeElement.querySelector('a').textContent).toContain(
+    //   'Flimbus'
+    // );
+
+    // same test as above, but using the debug element; exposes some additional properties
+    let debugElement = fixture.debugElement.query(By.css('a'));
+    expect(debugElement.nativeElement.textContent).toContain('Flimbus');
   });
 });
